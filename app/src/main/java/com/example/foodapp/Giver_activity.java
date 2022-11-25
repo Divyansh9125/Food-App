0f0ba1d31d6fb4163e36711b1508286d82b3e3ff
shiv836 <tshivansh2499@gmail.com>
2022-11-25 12:31:54 +0530

    I have created app

---
+++ app/src/main/java/com/example/foodapp/Giver_activity.java
@@ -0,0 +1,26 @@
+package com.example.foodapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Giver_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_giver);
+        Button button = findViewById(R.id.Submit_btn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Giver_activity.this,GiverMessage_activity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
