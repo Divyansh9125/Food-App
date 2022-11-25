940b4f3dd828430e1152ed4d3061ed6683259049
Divyansh9125 <divyanshsrivastava9125@gmail.com>
2022-11-25 15:56:46 +0530

    added apis

--- app/src/main/java/com/example/foodapp/UserDetails.java
+++ app/src/main/java/com/example/foodapp/UserDetails.java
@@ -4,8 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.IOException;
+
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class UserDetails extends AppCompatActivity {
 
@@ -17,9 +27,51 @@ public class UserDetails extends AppCompatActivity {
         userBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(UserDetails.this,MainActivity.class);
-                startActivity(intent);
+                Intent intent = new Intent(UserDetails.this, MainActivity.class);
+                TextView fnameView = findViewById(R.id.nameInput);
+                TextView lnameView = findViewById(R.id.name2Input);
+                TextView emailView = findViewById(R.id.emailInput);
+                TextView contactView = findViewById(R.id.favoriteNumberInput);
 
+                String fname = fnameView.getText().toString();
+                String lname = lnameView.getText().toString();
+                String email = emailView.getText().toString();
+                String contact = contactView.getText().toString();
+
+                OkHttpClient client = new OkHttpClient();
+
+                RequestBody formBody = new FormBody.Builder()
+                        .add("work_email", email)
+                        .add("fname", fname)
+                        .add("lname", lname)
+                        .add("contact", contact)
+                        .build();
+
+                Request request = new Request.Builder()
+                        .url("https://2f07-121-244-55-198.ngrok.io/foodapp/sharing/user/signup/")
+                        .post(formBody)
+                        .build();
+
+                Thread thread = new Thread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        try  {
+                            try{
+                                Response response = client.newCall(request).execute();
+                                System.out.println(response.body().toString());
+                                // Do something with the response.
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+
+                thread.start();
+                startActivity(intent);
             }
         });
     }
