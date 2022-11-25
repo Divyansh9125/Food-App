3729d749ac936c69f127b6c88abe5630efad85af
shiv836 <tshivansh2499@gmail.com>
2022-11-25 17:03:09 +0530

    added button for order
    added button

--- app/src/main/java/com/example/foodapp/Taker_details.java
+++ app/src/main/java/com/example/foodapp/Taker_details.java
@@ -2,7 +2,10 @@ package com.example.foodapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Taker_details extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class Taker_details extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_taker_details);
+        Button HomeBtn=findViewById(R.id.Home_Btn);
+        HomeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Taker_details.this,MainActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
